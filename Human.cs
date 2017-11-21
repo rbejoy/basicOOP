dcd53b6c0a6130bff2150dae180fddd4f8de0514
Renil Bejoy <renil.bejoy@vanderbilt.edu>
2017-11-20 20:03:29 -0800

    added basic OOP files

---
+++ Human.cs
@@ -0,0 +1,31 @@
+namespace basicOOP {
+    public class Human {
+        public string name;
+        public int strength = 3;
+        public int intelligence = 3;
+        public int dexterity = 3;
+        public int health = 100;
+
+        public Human(string n) {
+            name = n;
+        }
+
+        public Human(string n, int str, int intl, int dex, int hp){
+            name = n;
+            strength = str;
+            intelligence = intl;
+            dexterity = dex;
+            health = hp;
+        }
+
+        // public void Attack(Human enemy){
+        //     enemy.health -= 5 * strength;
+        // }
+        public void Attack(object target){
+            Human enemy = target as Human;
+            if(enemy != null) {
+                enemy.health -= 5 * strength;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace basicOOP
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Human player1 = new Human("Great Hero",20,8,16,500);
+            Human player2 = new Human("Random NPC");
+            player1.Attack(player2);
+            Console.WriteLine($"After being attacked by the {player1.name}, the {player2.name}'s health is now {player2.health}");
+        }
+    }
+}
